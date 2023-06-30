ff8895ef5cd4cab30e2e0d6387b3d8403f4f6b60
Mate Kamaras programming <matekamarasprogramming@mates-air.localdomain>
2023-07-01 01:03:08 +0200

    add for loop

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const tilesContainer = document.querySelector(".tiles");
 const colors = ["aqua","aquamarine","crimson","blue","dodgerblue","gold","greenyellow","teal",];
 const colorsPicklist = [...colors,...colors];
-console.log(colorsPicklist)
+const tileCount = colorsPicklist.length;
+
+
+
+
+for(let i = 0; i<tileCount; i++){
+    const randomIndex = Math.floor(Math.random()*colorsPicklist.length)
+    const color = colorsPicklist[randomIndex]
+    colorsPicklist.splice(randomIndex,1) 
+    console.log(color)   
+}
