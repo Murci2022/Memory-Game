278ab07373d68e9d8a69668b8905c4887f19c89b
Mate Kamaras programming <matekamarasprogramming@mates-air.localdomain>
2023-06-30 23:00:15 +0200

    created  var in  js file

---
+++ script.js
@@ -0,0 +1,4 @@
+const tilesContainer = document.querySelector(".tiles");
+const colors = ["aqua","aquamarine","crimson","blue","dodgerblue","gold","greenyellow","teal",];
+const colorsPicklist = [...colors,...colors];
+console.log(colorsPicklist)
